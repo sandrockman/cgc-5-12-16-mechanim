de9f6edea8e3e444236a97a918452b7da54d7877
svc-student <svc-student@student.directory.jccc>
2016-05-11 14:04:33 -0500

    done through video 5
    Start next on mechanim scripting aka video 6

---
+++ vhaskin1-cgc-mechanim/Assets/Scripts/MecanimControl.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MecanimControl : MonoBehaviour {
+
+	public Animator anim;
+	public Event eventScript;
+
+	// Use this for initialization
+	void Start () {
+
+		//anim = GetComponent<Animator> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Circle ();
+		Rotate ();
+		Jump ();
+		Wave ();
+	}
+
+
+	void Rotate ()
+	{
+		if (Input.GetButtonDown ("Fire1"))
+			anim.SetBool ("isRotated", true);
+		
+
+		if(Input.GetButtonDown("Fire2"))
+			anim.SetBool("isRotated", false);
+	}
+
+	void Circle ()
+	{
+		if (Input.GetKeyDown (KeyCode.A))
+			anim.SetInteger ("Circling", 1);
+				
+	    if (Input.GetKeyDown (KeyCode.D))
+			anim.SetInteger ("Circling", 2);
+		
+	}
+
+	void Jump ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		   anim.SetTrigger ("Jumping");	
+	}
+
+	void Wave ()
+	{
+		if (Input.GetKeyDown (KeyCode.W))
+			anim.SetFloat ("Waving", 0.2f);
+		if (Input.GetKeyDown (KeyCode.S))
+			anim.SetFloat ("Waving", -0.2f);
+		
+	
+	}
+}
+
