8a25e21b7cf635d214f0201bd9f7175afca98645
svc-student <svc-student@student.directory.jccc>
2016-05-11 22:31:22 -0500

    completed except for challenge.

---
+++ New Unity Project/Assets/Scripts/Event.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event : MonoBehaviour {
+
+	public Light sun;
+	public bool sunOn;
+
+	public void EventMessage ()
+	{
+		Debug.Log ("Hi, I'm a animation event");
+	}
+
+	public void Sunlight()
+	{
+		sunOn = !sunOn;
+		if (sunOn)
+			sun.enabled = true;
+		else
+			sun.enabled = false;
+	}
+}
---
+++ vhaskin1-cgc-mechanim/Assets/Gem/Script/Audio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audio : MonoBehaviour {
+	
+
+	public AudioSource aud;
+	public Animator anim;
+
+	void Update ()
+	{
+	
+		if (Input.GetButtonDown ("Fire1"))
+			anim.SetTrigger ("collect");
+	}
+
+	public void Pop ()
+	{
+		aud.Play ();
+		Destroy (gameObject, 2f);
+	}
+}
